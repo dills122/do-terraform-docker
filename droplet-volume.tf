320ef1eb57d7344c8bf5f6107eaf41b37f149eb5
Dylan Steele <dylansteele57@gmail.com>
2022-01-29 23:48:41 -0500

    test

--- droplet-volume.tf
+++ droplet-volume.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
-    }
-  }
-}
-
-provider "digitalocean" {}
-
 data template_file "userdata" {
   template = file("${path.module}/app.yaml")
 
